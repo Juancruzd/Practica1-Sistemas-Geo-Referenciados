4541a37301edc8543e4fd6e8bd6b7a074cf0ab64
Juancruzd <42796029+Juancruzd@users.noreply.github.com>
2020-02-25 18:06:22 -0600

    nueva practica 4 de api estatica de google(imagen de mapa)

---
+++ scriptpractica4.js
@@ -0,0 +1,14 @@
+function obtenerUbicacion(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(muestraPosicion)
+    }
+    else{
+        alert("no");
+    }
+    function muestraPosicion(position){
+        var coordenas= position.coords.latitude+","+position.coords.longitude;
+        console.log(coordenas);
+        var imagenurl="https://maps.googleapis.com/maps/api/staticmap?center="+coordenas+"&zoom=20&size=800x488&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyADjOfdGIg-7JiqjoTstMN9el4g-nLhxxA";
+        document.getElementById("mapa").innerHTML="<img src="+imagenurl+">";
+    }
+}
