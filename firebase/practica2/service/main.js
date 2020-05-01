f2338a39fc7052166ed6ee0cf523a0a7b8ed51e1
Juancruzd <42796029+Juancruzd@users.noreply.github.com>
2020-04-30 20:11:12 -0500

    cambios

--- firebase/practica2/service/main.js
+++ firebase/practica2/service/main.js
@@ -13,6 +13,9 @@ if(user){
        document.getElementById("linkmicuenta").style.display = 'block';
        document.getElementById("linkingresar").style.display = 'none';
        document.getElementById("linkregistro").style.display = 'none';
+       document.getElementById("linkingresar2").style.display = 'none';
+       document.getElementById("linkregistro2").style.display = 'none';
+       document.getElementById("divisioningeso").style.display = 'none';
     }
     else
     {
@@ -20,6 +23,11 @@ if(user){
      document.getElementById("linkmicuenta").style.display = 'none';
      document.getElementById("linkingresar").style.display = 'block';
      document.getElementById("linkregistro").style.display = 'block';
+     
+     document.getElementById("linkingresar2").style.display = 'block';
+     document.getElementById("linkregistro2").style.display = 'block';
+
+     document.getElementById("divisioningeso").style.display = 'block';
     } 
 });
 const salir = document.getElementById('linksalir');
