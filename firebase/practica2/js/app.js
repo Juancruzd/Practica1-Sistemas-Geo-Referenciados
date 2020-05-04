1b5facf057b80f2ceb754fd7af0fc8d8298df758
Juancruzd <42796029+Juancruzd@users.noreply.github.com>
2020-05-04 15:45:33 -0500

    hide show form search position scroll style /* Large desktop */ @media (min-width: 1200px) {      .navbar .navbar-brand img {          margin-left: 80px;     } }   @media (min-width: 768px) and (max-width: 1024px) {          .navbar .navbar-brand img {              margin-left: 80px;         } }

--- firebase/practica2/js/app.js
+++ firebase/practica2/js/app.js
@@ -5,7 +5,15 @@ $(document).ready(function() {
             document.getElementById("nav").setAttribute('style','border-bottom:1px solid #F4FCDE'); 
         }
         else{
-            document.getElementById("nav").setAttribute('style','border-bottom:1px solid #000'); 
+            document.getElementById("nav").setAttribute('style','border-bottom:1px solid #000');
+            console.log(scrollAmount); 
         }  
+
+
+        if (scrollAmount >= 415) { 
+            document.getElementById("formsearch").setAttribute('style','display: block');
+        }else{
+            document.getElementById("formsearch").setAttribute('style','display: none');
+        }
       });
  });
